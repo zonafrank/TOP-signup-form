193f1ddc8992a8a9e60b9eff589625480099dfb6
Eche <echyth@yahoo.com>
2023-01-10 15:03:23 +0100

    implement for validation and submission

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-alert("Hello!!!");
+const firstNameInput = document.getElementById("first-name");
+const lastNameInput = document.getElementById("last-name");
+const emailInput = document.getElementById("email");
+const phoneNumberInput = document.getElementById("phone-number");
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirm-password");
+const submitButton = document.getElementById("submit-button");
+const form = document.getElementById("form");
+const passwordErrorElem = document.getElementById("password-error");
+
+submitButton.addEventListener("click", submitForm);
+confirmPasswordInput.addEventListener("keyup", (e) => {
+  const { value } = e.target;
+  for (let i = 0; i < value.length; i++) {
+    if (value[i] !== passwordInput.value[i]) {
+      passwordErrorElem.innerText = "passwords do not match";
+      passwordErrorElem.classList.remove("hide");
+      passwordErrorElem.classList.add("show");
+    } else {
+      passwordErrorElem.innerText = "";
+      passwordErrorElem.classList.remove("show");
+      passwordErrorElem.classList.add("hide");
+    }
+  }
+});
+
+function validateFormInputs() {
+  let firstNameIsValid = firstNameInput.checkValidity();
+  let lastNameIsValid = lastNameInput.checkValidity();
+  let emailIsValid = emailInput.checkValidity();
+  let phoneNumberIsValid = phoneNumberInput.checkValidity();
+  let passwordIsValid = phoneNumberInput.checkValidity();
+  let confirmPasswordIsValid = confirmPasswordInput.checkValidity();
+  let passwordConfirmationPassed =
+    passwordInput.value === confirmPasswordInput.value;
+
+  const allChecks = [
+    firstNameIsValid,
+    lastNameIsValid,
+    emailIsValid,
+    phoneNumberIsValid,
+    passwordIsValid,
+    confirmPasswordIsValid,
+    passwordConfirmationPassed
+  ];
+
+  return allChecks.every((item) => item);
+}
+
+function submitForm(e) {
+  e.preventDefault();
+  const inputsAreValid = validateFormInputs();
+  if (inputsAreValid) {
+    form.submit();
+  } else {
+    throw new Error("Invalid input");
+  }
+}
